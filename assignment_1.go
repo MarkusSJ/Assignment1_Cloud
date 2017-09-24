6750e2481c59b1cf20eaffe20aa4bf878f5abf7b
MarkusSJ <markussj@stud.ntnu.no>
2017-09-24 12:05:42 +0200

    used gofmt to format code proerly

--- assignment_1.go
+++ assignment_1.go
@@ -1,93 +1,96 @@
 package main
 
-import(
-   "encoding/json"
-   "net/http"
-   "strings"
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
 )
+
 // Project struct for output
 type Project struct {
-	Repos           string `json:"project"`
-	Owner		string `json:"owner"`
-	Committer	string `json:"committer"`
-	Commits		int    `json:"commits"`
-	Languages	[]string `json:"language"`
+	Repos     string   `json:"project"`
+	Owner     string   `json:"owner"`
+	Committer string   `json:"committer"`
+	Commits   int      `json:"commits"`
+	Languages []string `json:"language"`
 }
+
 // Contributors struct for the contributrs API
 type Contributors struct {
-	Login	        string `json:"login"`
-	Contributions   int `json:"contributions"`
+	Login         string `json:"login"`
+	Contributions int    `json:"contributions"`
 }
+
 // Repos struct for the Repository API
 type Repos struct {
-	Name		string `json:"name"`
-        Owner	struct{
-		   Login	 string `json:"login"`
-		}
+	Name  string `json:"name"`
+	Owner struct {
+		Login string `json:"login"`
+	}
 }
 
 // GetContent was taken from the first answer on the question How to get JSON response in Golang: https://stackoverflow.com/questions/17156371/how-to-get-json-response-in-golang
 
-func getContent(url string, target interface{})error {
+func getContent(url string, target interface{}) error {
 
-   res, err := http.Get(url)
-   if err != nil{
-      return err
-   }
-   defer res.Body.Close()
-   return json.NewDecoder(res.Body).Decode(target)
+	res, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	return json.NewDecoder(res.Body).Decode(target)
 }
 
-func handlerProjects(w http.ResponseWriter, r *http.Request){
-   http.Header.Add(w.Header(), "content-type", "application/json")
+func handlerProjects(w http.ResponseWriter, r *http.Request) {
+	http.Header.Add(w.Header(), "content-type", "application/json")
 
-   arg := r.URL.Path
-   s := strings.Split(arg, "/")
-   url := "https://api.github.com/repos/" + s[4] + "/" +s[5]
-   langurl := url + "/languages"
-   contrurl := url + "/contributors"
+	arg := r.URL.Path
+	s := strings.Split(arg, "/")
+	url := "https://api.github.com/repos/" + s[4] + "/" + s[5]
+	langurl := url + "/languages"
+	contrurl := url + "/contributors"
 
-   ra := make(map[string]interface{})
+	ra := make(map[string]interface{})
 
-   t1 := &Repos{}
-   t2 := &[]Contributors{}
+	t1 := &Repos{}
+	t2 := &[]Contributors{}
 
-   t  := &Project{}
+	t := &Project{}
 
-   err := getContent(url, t1)
-   if err != nil {
-      http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-      return
-   }
+	err := getContent(url, t1)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
 
-   err = getContent(contrurl, t2)
-   if err != nil {
-       http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-      return
-   }
+	err = getContent(contrurl, t2)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
 
-   err = getContent(langurl, &ra)
-      if err != nil {
-        http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-      return
-   }
+	err = getContent(langurl, &ra)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
 
-   t.Repos = t1.Name
-   t.Owner = t1.Owner.Login
-   t.Committer = (*t2)[0].Login
-   t.Commits = (*t2)[0].Contributions
+	t.Repos = t1.Name
+	t.Owner = t1.Owner.Login
+	t.Committer = (*t2)[0].Login
+	t.Commits = (*t2)[0].Contributions
 
-   lang := []string{}
-    for i := range ra{
-      lang = append(lang, i)
-   }
+	lang := []string{}
+	for i := range ra {
+		lang = append(lang, i)
+	}
 
-   t.Languages = lang
+	t.Languages = lang
 
-   json.NewEncoder(w).Encode(t)
+	json.NewEncoder(w).Encode(t)
 }
 
-func main(){
-   http.HandleFunc("/projectinfo/v1/", handlerProjects)
-   http.ListenAndServe(":8080", nil)
+func main() {
+	http.HandleFunc("/projectinfo/v1/", handlerProjects)
+	http.ListenAndServe(":8080", nil)
 }
